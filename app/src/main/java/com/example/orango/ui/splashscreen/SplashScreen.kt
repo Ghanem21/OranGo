cb2b0525114b66dadbb8b93d1d6d9d28679a4dc4
Abdulrahman Mohammad Mahmoud Ragab <76666355+Abdulrahman1890@users.noreply.github.com>
2023-02-07 18:09:01 +0200

    suspend function for the delay and I made the top img as a background

--- app/src/main/java/com/example/orango/ui/splashscreen/SplashScreen.kt
+++ app/src/main/java/com/example/orango/ui/splashscreen/SplashScreen.kt
@@ -10,6 +10,10 @@ import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.example.orango.R
 import com.example.orango.databinding.FragmentSplashScreenBinding
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class SplashScreen : Fragment(){
     private var _binding: FragmentSplashScreenBinding? = null
@@ -33,12 +37,12 @@ class SplashScreen : Fragment(){
         binding.logo.startAnimation(fadedAnim)
         binding.qoute.startAnimation(fadedAnim)
 
-        // we used the postDelayed(Runnable, time) method
-        // to send a message with a delayed time.
-        @Suppress("DEPRECATION")
-        Handler().postDelayed({
-            findNavController().navigate(R.id.action_splashScreen_to_onBoardingFragment)
-        }, 4000) // 4000 is the delayed time in milliseconds.
-
+        GlobalScope.launch(Dispatchers.Main) {
+            delayedTimeForFragment()
+        }
+    }
+    private suspend fun delayedTimeForFragment(){
+        delay(4000)
+        findNavController().navigate(R.id.action_splashScreen_to_onBoardingFragment)
     }
 }
