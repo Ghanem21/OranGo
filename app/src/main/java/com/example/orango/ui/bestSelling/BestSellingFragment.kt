3434005f7a59fc9c04821b8712a6302341c07ebc
abdel <ghanem.ana.abdo@gmail.com>
2023-03-01 06:32:47 +0200

    test

--- app/src/main/java/com/example/orango/ui/bestSelling/BestSellingFragment.kt
+++ app/src/main/java/com/example/orango/ui/bestSelling/BestSellingFragment.kt
@@ -13,7 +13,7 @@ class BestSellingFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_best_selling, container, false)
     }
