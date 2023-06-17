9e13e58025a8937b5bb278d6131ef7b705d8ae54
abdel <ghanem.ana.abdo@gmail.com>
2023-06-17 20:08:09 +0300

    App until Auth

--- app/src/main/java/com/example/orango/ui/signup/SignUpViewModel.kt
+++ app/src/main/java/com/example/orango/ui/signup/SignUpViewModel.kt
@@ -1,6 +1,7 @@
 package com.example.orango.ui.signup
 
 import android.app.Application
+import android.widget.Toast
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
@@ -9,16 +10,22 @@ import com.example.data.repo.RepoImpl
 import com.example.data.roomDB.OranGoDataBase
 import kotlinx.coroutines.launch
 
-class SignUpViewModel (application: Application) : AndroidViewModel(application) {
+class SignUpViewModel(application: Application) : AndroidViewModel(application) {
 
-    private val database = OranGoDataBase.getInstance(application.applicationContext)
-    private val repo = RepoImpl(database)
+    private val repo: RepoImpl
 
     private val emailErrorLiveData = MutableLiveData<String?>()
-    val emailError : LiveData<String?> = emailErrorLiveData
+    val emailError: LiveData<String?> = emailErrorLiveData
 
     private val phoneNumberErrorLiveData = MutableLiveData<String?>()
-    val phoneNumberError : LiveData<String?> = phoneNumberErrorLiveData
+    val phoneNumberError: LiveData<String?> = phoneNumberErrorLiveData
+
+    private val signUpSucceedLiveData = MutableLiveData<Boolean>()
+    val signUpSucceed: LiveData<Boolean> = signUpSucceedLiveData
+    init {
+        val database = OranGoDataBase.getInstance(application.applicationContext)
+        repo = RepoImpl(database)
+    }
 
     fun validateEmail(email: String): Boolean {
         if (email.isEmpty()) {
@@ -26,55 +33,53 @@ class SignUpViewModel (application: Application) : AndroidViewModel(application)
         }
 
         val emailRegex = Regex("[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}")
-        if (!emailRegex.matches(email)) {
-            return false
-        }
-
-        return true
+        return emailRegex.matchEntire(email) != null
     }
 
     fun validatePassword(password: String): Boolean {
-        if (password.length < 8) {
-            return false
-        }
-        return true
+        return password.length >= 8
     }
 
     fun validatePhoneNumber(phoneNumber: String): Boolean {
-        if (phoneNumber.length < 11) {
-            return false
-        }
-        return true
+        return phoneNumber.length >= 11
     }
 
     fun validateUsername(username: String): Boolean {
-        if (username.length < 3) {
-            return false
-        }
-        return true
+        return username.length >= 3
     }
 
-    fun signUp(username:String,email: String,phoneNumber:String, password: String) {
+    fun signUp(username: String, email: String, phoneNumber: String, password: String) {
         viewModelScope.launch {
             try {
-                val status = repo.signUp(username,email,phoneNumber, password)
+                val status = repo.signUp(username, email, phoneNumber, password)
                 if (!status) {
-                    handleError()
+                    handleError("Sign up failed")
                     return@launch
                 }
+                handleSuccess()
             } catch (ex: Exception) {
+                handleError(ex.message ?: "An error occurred")
                 ex.printStackTrace()
             }
         }
     }
 
-    private fun handleError() {
-        repo.signUPError?.email?.getOrNull(0)?.let {
-            emailErrorLiveData.value = it
-        }
+    private fun handleSuccess() {
+        emailErrorLiveData.value = null
+        phoneNumberErrorLiveData.value = null
+        signUpSucceedLiveData.value = true
+    }
 
-        repo.signUPError?.phoneNumber?.getOrNull(0)?.let {
-            phoneNumberErrorLiveData.value = it
+    private fun handleError(errorMessage: String) {
+        when (errorMessage) {
+            "Email error" -> emailErrorLiveData.value = "Invalid email"
+            "PhoneNumber error" -> phoneNumberErrorLiveData.value = "Invalid phone number"
+            else -> {
+                Toast.makeText(getApplication(), errorMessage, Toast.LENGTH_LONG).show()
+            }
         }
+    }
+    fun signUpDone() {
+        signUpSucceedLiveData.value = false
     }
 }
