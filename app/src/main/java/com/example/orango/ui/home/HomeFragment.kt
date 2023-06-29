ed57fac5651e71bccc773fdc95b3b50944215b48
abdel <ghanem.ana.abdo@gmail.com>
2023-06-29 08:57:30 +0300

    Toast exc

--- app/src/main/java/com/example/orango/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/orango/ui/home/HomeFragment.kt
@@ -1,6 +1,7 @@
 package com.example.orango.ui.home
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -92,6 +93,7 @@ class HomeFragment : Fragment() {
 
         viewModel.savedCustomerData.observe(viewLifecycleOwner) { userData ->
             binding.headerLayout.usernameText.text = "Hi, ${userData.user.user_name}"
+            Log.d("TAG", "observeData: ${userData.user.image}")
             Glide.with(requireContext())
                 .load(userData.user.image)
                 .centerCrop()
